2c492f5484ed0b132f575b901434cda00710b5e1
xhy <xuhy5@asiainfo.com>
2020-03-04 15:50:39 +0800

    git工具提交测试

---
+++ Controller.java
@@ -0,0 +1,29 @@
+package controller;
+
+import core.RandomValidateCode;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Controller {
+    @RequestMapping(value="/checkCode")
+    public void checkCode(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //设置相应类型,告诉浏览器输出的内容为图片
+        response.setContentType("image/jpeg");
+
+        //设置响应头信息，告诉浏览器不要缓存此内容
+        response.setHeader("pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expire", 0);
+
+        RandomValidateCode randomValidateCode = new RandomValidateCode();
+        try {
+            randomValidateCode.getRandcode(request, response);//输出图片方法
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
